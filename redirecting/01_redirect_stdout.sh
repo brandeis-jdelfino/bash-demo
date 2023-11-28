098283eb758fb29406927d3875cb98c9e703c60e
Joe Delfino <jdelfino@gmail.com>
2023-11-28 18:09:21 +0000

    fix

--- redirecting/01_redirect_stdout.sh
+++ redirecting/01_redirect_stdout.sh
@@ -3,10 +3,10 @@
 # all commands have 2 output streams: stdout and stderr
 
 # '>' redirects stdout to a file, overwriting whatever was there
-grep -i "copyright" ../LICENSE > matches.txt
+grep -i "copyright" LICENSE > matches.txt
 
 # '>>' redirects stdout to a file, appending to whatever was there
-grep -i "documentation" ../LICENSE >> matches.txt
+grep -i "documentation" LICENSE >> matches.txt
 
 echo "Contents of matches.txt:"
 cat matches.txt
